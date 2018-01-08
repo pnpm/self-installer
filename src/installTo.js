adff0cc5ceca32da5e816cf235f676359535d421
Zoltan Kochan <z@kochan.io>
2018-01-08 03:06:36 +0200

    feat: install by spec

--- src/installTo.js
+++ src/installTo.js
@@ -6,7 +6,8 @@ const got = require('got')
 const unpackStream = require('unpack-stream')
 const getNpmTarballUrl = require('get-npm-tarball-url').default
 const cmdShim = require('cmd-shim')
-const pnpmPkg = require('../../../package.json')
+const createResolver = require('@pnpm/npm-resolver').default
+const tempy = require('tempy')
 
 module.exports = installTo
 
@@ -16,26 +17,42 @@ function installTo (dest, binPath) {
   const pnpmBin = path.join(pnpm, 'lib/bin/pnpm.js')
   const pnpxBin = path.join(pnpm, 'lib/bin/pnpx.js')
 
-  const tarball = getNpmTarballUrl(pnpmPkg.bundledName || pnpmPkg.name, pnpmPkg.version)
-  console.log(`Downloading ${tarball}`)
-  const stream = got.stream(tarball)
-  return unpackStream.remote(stream, pnpm)
-    .then(index => {
-      return new Promise((resolve, reject) => {
-        cmdShim(pnpmBin, path.join(binPath, 'pnpm'), (err) => {
-          if (err) {
-            reject(err)
-            return
-          }
-          cmdShim(pnpxBin, path.join(binPath, 'pnpx'), (err) => {
+  const registry = 'https://registry.npmjs.org/'
+  const resolvePackage = createResolver({
+    registry,
+    rawNpmConfig: { registry },
+    metaCache: new Map(),
+    dryRun: true, // nothing will be written to the store
+    store: tempy.directory(), // a silly hack because store is required
+  })
+  return resolvePackage({alias: 'pnpm', pref: 'latest'}, {registry})
+    .then(res => {
+      return resolvePackage({alias: '@pnpm/bundled', pref: res.package.version}, {registry})
+        .then(bundledRes => downloadTarball(bundledRes.resolution.tarball))
+        .catch(err => downloadTarball(res.resolution.tarball))
+    })
+
+  function downloadTarball (tarball) {
+    console.log(`Downloading ${tarball}`)
+    const stream = got.stream(tarball)
+    return unpackStream.remote(stream, pnpm)
+      .then(index => {
+        return new Promise((resolve, reject) => {
+          cmdShim(pnpmBin, path.join(binPath, 'pnpm'), (err) => {
             if (err) {
               reject(err)
               return
             }
-            spawnSync('node', [npmBin, 'rebuild', 'drivelist'], {cwd: pnpm, stdio: 'inherit'})
-            resolve()
+            cmdShim(pnpxBin, path.join(binPath, 'pnpx'), (err) => {
+              if (err) {
+                reject(err)
+                return
+              }
+              spawnSync('node', [npmBin, 'rebuild', 'drivelist'], {cwd: pnpm, stdio: 'inherit'})
+              resolve()
+            })
           })
         })
       })
-    })
+  }
 }
