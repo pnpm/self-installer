c5224ac8bdbc013e6c0670d8ee19e9b32955b01f
Zoltan Kochan <z@kochan.io>
2018-05-14 23:04:36 +0300

    fix: always install pnpm@latest-1 on Node.js 4

--- src/install.js
+++ src/install.js
@@ -5,10 +5,21 @@ const installTo = require('./installTo')
 installTo({
     dest: process.env.PNPM_DEST && path.resolve(process.env.PNPM_DEST),
     binPath: process.env.PNPM_BIN_DEST && path.resolve(process.env.PNPM_BIN_DEST),
-    version: process.env.PNPM_VERSION || 'latest',
+    version: process.env.PNPM_VERSION || getDefaultVersion(),
     registry: process.env.PNPM_REGISTRY || 'https://registry.npmjs.org/',
   })
   .catch(err => {
     console.error(err)
     process.exit(1)
   })
+
+function getDefaultVersion () {
+  const nodeMajor = require('semver').major(process.version)
+  switch (nodeMajor) {
+    case 4:
+      return 'latest-1'
+    default:
+      if (nodeMajor < 4) throw new Error('pnpm requires at least Node.js v4')
+      return 'latest'
+  }
+}
